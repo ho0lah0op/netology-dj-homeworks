5da2a603eaceebcfec07a59f0af30e84a82cb468
Louiz Hounas <hlouiz@yandex.ru>
2023-12-17 13:38:06 +0300

    3.1-drf-intro updated - migration file added, settings updated

--- 3.1-drf-intro/smart_home/smart_home/settings.py
+++ 3.1-drf-intro/smart_home/smart_home/settings.py
@@ -80,11 +80,11 @@ WSGI_APPLICATION = 'smart_home.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_smart_home_2',
+        'NAME': 'netology_smart_home',
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'USER': 'postgres',
-        'PASSWORD': 'hunas',
+        'PASSWORD': '',
     }
 }
 
